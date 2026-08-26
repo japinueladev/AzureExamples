bc5cfc104bd595b688388f750a63bc1b0c98711e
Juan A. Piñuela <japinuela@yahoo.es>
2026-08-26 20:15:06 +0200

    Iniciado ContainerApp

---
+++ ContainerApp/app.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hola mundo"
+
+@app.route('/profile/')
+def profile(): 
+    return {"name": "Juan Pérez", "profile_photo_url": "https://picsum.photos/200"} 
+
+if __name__ == '__main__': 
+    app.run(host="0.0.0.0", port=8080, debug=True)
